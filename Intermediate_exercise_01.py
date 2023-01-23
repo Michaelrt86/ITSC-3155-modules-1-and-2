9e6d4441a1775ff4313f83e038c26b743bdb62b0
Michaelrt86 <Michaelrt867@gmail.com>
2023-01-23 16:30:57 -0500

    Intermediate Python Exercises 1 through 3

--- Intermediate_exercise_01.py
+++ Intermediate_exercise_01.py
@@ -1,3 +1,9 @@
+#Intermediate Exercise 01
+# Write a function that takes in a list data structure as input. The function should create a new 
+# list and only include unique elements of the inputted list. Under the function, write code that 
+# calls the function with a test list like so and print out the result. 
+# Remember that your code should work for all lists of integers, not just the sample test here.
+
 def get_unique_list(tempList):
     newList = tempList
     y = 0
--- Intermediate_exercise_02.py
+++ Intermediate_exercise_02.py
@@ -1,3 +1,11 @@
+#Intermediate Exercise 02
+#Write a function that takes in two dict data structures as input. 
+# Both dicts map str->int (the keys are strings and the values are integers). 
+# The function should compute a new dict which combines the two dicts by summing 
+# the values for the common keys. Keys that are not common should be left out. 
+# Use the following code to test your function, but remember that your function 
+# should for all str->int dict inputs, not just the test here.
+
 def get_combined_dict(list1, list2):
     thirdDict = {}
 
