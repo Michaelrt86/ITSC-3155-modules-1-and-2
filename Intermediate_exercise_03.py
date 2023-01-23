6385bb6b90b451571afa26b6e115e99b3fe39b91
Michaelrt86 <Michaelrt867@gmail.com>
2023-01-23 16:31:03 -0500

    Intermediate_exercise_03 Commited!

--- Intermediate_exercise_03.py
+++ Intermediate_exercise_03.py
@@ -1,3 +1,8 @@
+#Intermediate Exercise 03
+#Take in a string from the user and pass it as input to a function. 
+# Have the function return a dict which keeps count of each letter 
+# (in lowercase) in the string, excluding spaces. Print out this dict.
+
 def functionReturn(tempString):
     userInput = tempString.lower().replace(' ', '')
     dictList = {}
